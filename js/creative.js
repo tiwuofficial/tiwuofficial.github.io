cfb2abef66a5814bc26a35b2c3e7948f8fe1ceaf
System Administrator <root@wataru.local>
2016-02-28 22:57:22 +0900

    cssの非同期

--- js/creative.js
+++ js/creative.js
@@ -46,3 +46,23 @@
     new WOW().init();
 
 })(jQuery); // End of use strict
+
+function loadCss(href, check) {
+
+    if(typeof(check) == 'undefined') check = true;
+
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = href;
+
+    if(check) {
+        var links = head.getElementsByTagName('link');
+        for(var i = 0; i < links.length; i++) {
+            if(links[i].href == link.href) return false;
+        }
+    }
+
+    head.appendChild(link);
+}
